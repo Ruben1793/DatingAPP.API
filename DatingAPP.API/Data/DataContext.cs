1ddbbe3f1351b94557004bfe5690d55eb9eeebac
Ruben <ruben.ojed9a3@gmail.com>
2019-02-06 21:17:12 -0600

    Add project files.

---
+++ DatingAPP.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DatingAPP.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}

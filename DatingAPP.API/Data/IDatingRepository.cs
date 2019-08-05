456bd250b0d628f406cffcb231e55986f9d33b42
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-08-04 21:48:23 -0500

    Creating new Repository for API

---
+++ DatingAPP.API/Data/IDatingRepository.cs
@@ -0,0 +1,18 @@
+﻿using DatingAPP.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingAPP.API.Data
+{
+    interface IDatingRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> saveAll();
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUser(int id);
+
+    }
+}

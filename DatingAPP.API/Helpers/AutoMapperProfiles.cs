500ec9eccfa272f38079295d7caeb6ce42ae381a
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-10-22 19:07:45 -0500

    Updating Register Method in API

--- DatingAPP.API/Helpers/AutoMapperProfiles.cs
+++ DatingAPP.API/Helpers/AutoMapperProfiles.cs
@@ -34,6 +34,8 @@ namespace DatingAPP.API.Helpers
             CreateMap<Photo, PhotoForReturnDto>();
 
             CreateMap<PhotoForCreationDto, Photo>();
+
+            CreateMap<UserForRegisterDto, User>();
         }
     }
 }

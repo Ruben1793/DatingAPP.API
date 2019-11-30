245af82956a6e64da6a849fa13b5d6177c8f903f
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-11-29 19:44:47 -0600

    Adding repository methods for the messages

--- DatingAPP.API/Data/DatingRepository.cs
+++ DatingAPP.API/Data/DatingRepository.cs
@@ -86,10 +86,24 @@ namespace DatingAPP.API.Data
             }
         }
 
-
         public async Task<bool> SaveAll()
         {
             return await _context.SaveChangesAsync() > 0 ;
+        }
+
+        public async Task<Message> GetMessage(int id)
+        {
+            return await _context.Messages.FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public Task<PagedList<Message>> GetMessagesForUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
